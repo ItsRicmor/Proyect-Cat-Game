d288e3579015162c81fd4209b2e274db7e195e11
Arlen <arlenvargas2000@gmail.com>
2019-04-19 13:21:10 -0600

    js

---
+++ JS/scripts.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+   
+        $('.box').on('click', function(){
+            let hasClassX = $(this).hasClass('img-x')
+            if(hasClassX){
+                $(this).removeClass('img-x')
+                $(this).addClass('img-o')
+            }else {
+                $(this).removeClass('img-o')
+                $(this).addClass('img-x')
+            }
+        })
+});
